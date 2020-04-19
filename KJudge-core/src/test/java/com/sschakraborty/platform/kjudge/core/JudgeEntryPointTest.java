2080bd072d3e609201f6c9b26c5e920c8d564a93
sschakraborty <sschakraborty@hotmail.com>
2020-04-19 14:31:35 +0530

    Minor bug fix!

--- KJudge-core/src/test/java/com/sschakraborty/platform/kjudge/core/JudgeEntryPointTest.java
+++ KJudge-core/src/test/java/com/sschakraborty/platform/kjudge/core/JudgeEntryPointTest.java
@@ -1,11 +1,13 @@
 package com.sschakraborty.platform.kjudge.core;
 
 import com.sschakraborty.platform.kjudge.error.AbstractBusinessException;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class JudgeEntryPointTest {
 	@Test
 	public void performJudgement() throws AbstractBusinessException {
 		JudgeEntryPoint entryPoint = new JudgeEntryPoint();
+		Assert.assertTrue(entryPoint != null);
 	}
 }
--- KJudge-core/src/test/java/com/sschakraborty/platform/kjudge/core/judge/java/Java8CoreJudgeTest.java
+++ KJudge-core/src/test/java/com/sschakraborty/platform/kjudge/core/judge/java/Java8CoreJudgeTest.java
@@ -1,5 +1,6 @@
 package com.sschakraborty.platform.kjudge.core.judge.java;
 
+import com.sschakraborty.platform.kjudge.core.io.PropertyFileReader;
 import com.sschakraborty.platform.kjudge.error.AbstractBusinessException;
 import com.sschakraborty.platform.kjudge.shared.model.*;
 import org.junit.Test;
@@ -7,11 +8,15 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class Java8CoreJudgeTest {
+	private final Properties masterProperties;
 	private final Java8CoreJudge judge;
 
 	public Java8CoreJudgeTest() throws AbstractBusinessException {
+		PropertyFileReader propertyFileReader = new PropertyFileReader();
+		this.masterProperties = propertyFileReader.readPropertiesUsingFileName("kjudge.properties");
 		judge = new Java8CoreJudge();
 	}
 
@@ -25,9 +30,13 @@ public class Java8CoreJudgeTest {
 			"    }\n" +
 			"}");
 
-		Testcase testcase = new Testcase();
-		testcase.setName("TC1");
-		testcase.setInputFilePath("/home/sschakraborty/Documents/exper/input");
+		Testcase tc1 = new Testcase();
+		tc1.setName("TC1");
+		tc1.setInputFilePath(this.masterProperties.getProperty("kjudge.stageDirectory") + "/input");
+
+		Testcase tc2 = new Testcase();
+		tc2.setName("TC2");
+		tc2.setInputFilePath(this.masterProperties.getProperty("kjudge.stageDirectory") + "/input");
 
 		Map<Language, Integer> map = new HashMap<>();
 		map.put(Language.JAVA_8, 1000);
@@ -36,7 +45,7 @@ public class Java8CoreJudgeTest {
 
 		Problem problem = new Problem();
 		problem.setProblemHandle("HelloWorld123");
-		problem.setTestcases(Arrays.asList(testcase));
+		problem.setTestcases(Arrays.asList(tc1, tc2));
 		problem.setTimeConstraint(timeConstraint);
 
 		User user = new User();
