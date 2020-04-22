27ddf23d589ee3bd60e84e6365ee83ee29712ab2
sschakraborty <sschakraborty@hotmail.com>
2020-04-22 20:59:51 +0530

    Improvement in Submission.

--- KJudge-shared/src/main/java/com/sschakraborty/platform/kjudge/shared/model/SubmissionResult.java
+++ KJudge-shared/src/main/java/com/sschakraborty/platform/kjudge/shared/model/SubmissionResult.java
@@ -3,6 +3,9 @@ package com.sschakraborty.platform.kjudge.shared.model;
 import java.util.List;
 
 public class SubmissionResult {
+	private Problem problem;
+	private Submission submission;
+
 	private List<SubmissionResultUnit> resultUnits;
 
 	public List<SubmissionResultUnit> getResultUnits() {
@@ -11,5 +14,21 @@ public class SubmissionResult {
 
 	public void setResultUnits(List<SubmissionResultUnit> resultUnits) {
 		this.resultUnits = resultUnits;
+	}
+
+	public Problem getProblem() {
+		return problem;
+	}
+
+	public void setProblem(Problem problem) {
+		this.problem = problem;
+	}
+
+	public Submission getSubmission() {
+		return submission;
+	}
+
+	public void setSubmission(Submission submission) {
+		this.submission = submission;
 	}
 }
