bf054af7f58c5662571fc31f3c48b3ca82cde4c5
sschakraborty <sschakraborty@hotmail.com>
2020-05-12 07:46:37 +0530

    Changed CreateProblemHandler

--- KJudge-service/src/main/java/com/sschakraborty/platform/kjudge/service/handlers/protectedHandler/handler/CreateProblemHandler.java
+++ KJudge-service/src/main/java/com/sschakraborty/platform/kjudge/service/handlers/protectedHandler/handler/CreateProblemHandler.java
@@ -4,13 +4,13 @@ import com.sschakraborty.platform.kjudge.data.GenericDAO;
 import com.sschakraborty.platform.kjudge.error.logger.LoggingUtility;
 import com.sschakraborty.platform.kjudge.service.handlers.AbstractRouteHandler;
 import com.sschakraborty.platform.kjudge.shared.model.*;
-import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.common.template.TemplateEngine;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class CreateProblemHandler extends AbstractRouteHandler {
 	public CreateProblemHandler(GenericDAO genericDAO, TemplateEngine templateEngine) {
@@ -52,7 +52,11 @@ public class CreateProblemHandler extends AbstractRouteHandler {
 			try {
 				final JsonObject creationData = model.getJsonObject("creationData");
 
-				final Problem problem = new Problem();
+				Problem problem = getGenericDAO().fetchFull(Problem.class, creationData.getString("problemHandle"));
+				if (problem == null) {
+					problem = new Problem();
+				}
+
 				problem.setProblemHandle(creationData.getString("problemHandle"));
 				problem.setCodingEvent(getGenericDAO().fetchFull(CodingEvent.class, creationData.getString("codingEventHandle")));
 				problem.setName(creationData.getString("name"));
@@ -60,20 +64,35 @@ public class CreateProblemHandler extends AbstractRouteHandler {
 				problem.setTestcases(Arrays.asList());
 				problem.setSolutions(Arrays.asList());
 
-				for(Object object : creationData.getJsonArray("testcases")) {
+				for (Object object : creationData.getJsonArray("testcases")) {
 					final JsonObject testcaseObject = (JsonObject) object;
 					final Testcase testcase = testcaseObject.mapTo(Testcase.class);
 					problem.getTestcases().add(testcase);
 				}
 
-				for(Object object : creationData.getJsonArray("solutions")) {
+				for (Object object : creationData.getJsonArray("solutions")) {
 					final JsonObject solutionObject = (JsonObject) object;
 					final CodeSubmission codeSubmission = solutionObject.mapTo(CodeSubmission.class);
 					problem.getSolutions().add(codeSubmission);
 				}
 
+				final HashMap ioConstraints = creationData.getJsonObject("ioConstraints").mapTo(HashMap.class);
+				final IOConstraint ioConstraint = new IOConstraint();
+				ioConstraint.setOutputLimits(ioConstraints);
+				problem.setIoConstraint(ioConstraint);
+
+				final HashMap memoryConstraints = creationData.getJsonObject("memoryConstraints").mapTo(HashMap.class);
+				final MemoryConstraint memoryConstraint = new MemoryConstraint();
+				memoryConstraint.setMemoryConstraints(memoryConstraints);
+				problem.setMemoryConstraint(memoryConstraint);
+
+				final HashMap timeConstraints = creationData.getJsonObject("timeConstraints").mapTo(HashMap.class);
+				final TimeConstraint timeConstraint = new TimeConstraint();
+				timeConstraint.setTimeConstraints(timeConstraints);
+				problem.setTimeConstraint(timeConstraint);
+
 				getGenericDAO().saveOrUpdate(problem);
-			} catch(Exception e) {
+			} catch (Exception e) {
 				LoggingUtility.logger().error(e);
 				routingContext.fail(404);
 			}
